29ef2fa5322d738912fefc59f198e87955dfd2f0
Brady Berg <Bergbrae@msu.edu>
2024-06-05 14:58:33 -0400

    Add sidebar and add files button

--- src/App.js
+++ src/App.js
@@ -1,28 +1,44 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Tabs, Tab } from 'react-bootstrap';
+import Nav from 'react-bootstrap/Nav';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import './App.css';
 import { DataProvider } from './components/context';
 import Pdf from './components/Pdf';
 import Pdfs from './components/Pdfs';
 import Sortkey from './components/Sortkey';
 import Home from './components/Home';
+import Pdftools from './components/Pdftools';
 
 function App() {
   return (
     <div className="App theme-dark">
       <DataProvider>
-        <Tabs defaultActiveKey="home" id="uncontrolled-tab-example" className="mb-3 sticky-top bg-white">
-          <Tab eventKey="home" title="Home">
-            <Home />
-          </Tab>
-          <Tab eventKey="pdfs" title="PDFs">
-            <Pdfs />
-          </Tab>
-          <Tab eventKey="sortkey" title="Sort Key">
-            <Sortkey />
-          </Tab>
-        </Tabs>
+        <Tab.Container id="left-tabs" defaultActiveKey="PDFs">
+          <Row>
+            <Col sm={2} className="sidebar p-0 m-0">
+              <h1>PDFBuilder</h1>
+              <Nav variant="pills" className="flex-column" style={{width: "100%"}}>
+                <Nav.Item className='m-2 mb-0'>
+                  <Nav.Link eventKey="PDFs">PDFs</Nav.Link>
+                </Nav.Item>
+                <Nav.Item className='m-2'>
+                  <Nav.Link eventKey="sort_key">Sort Key</Nav.Link>
+                </Nav.Item>
+              </Nav>
+              <hr className='m-4 mt-2 mb-5'/>
+              <Tab.Pane eventKey="PDFs" style={{bottom: 0}}><Pdftools /></Tab.Pane>
+            </Col>
+            <Col sm={10} className="content">
+              <Tab.Content>
+                <Tab.Pane eventKey="PDFs"><Pdfs /></Tab.Pane>
+                <Tab.Pane eventKey="sort_key"><Sortkey /></Tab.Pane>
+              </Tab.Content>
+            </Col>
+          </Row>
+        </Tab.Container>
       </DataProvider>
     </div>
   );
---
+++ src/components/Pdftools.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Col, Row, Container, Button, FormControl } from 'react-bootstrap';
+import { DataContext } from './context';
+
+export default function Pdftools() {
+    const [data, setData] = React.useContext(DataContext);
+    const files = data.pdfs.files;
+    const fileInputRef = React.useRef(null);
+
+    const handleFileChange = (e) => {
+        const files = e.target.files;
+        setData((prevData) => {
+            return {
+                ...prevData,
+                pdfs: {
+                    ...prevData.pdfs,
+                    files: [...prevData.pdfs.files, ...files],
+                },
+            };
+        });
+    };
+
+    return (
+        <Container>
+            <Row className='align-items-center'>
+                <Col>
+                    <Button onClick={() => fileInputRef.current.click()}>Add Files</Button>
+                    <FormControl
+                        type="file"
+                        id="file"
+                        multiple
+                        onChange={handleFileChange}
+                        ref={fileInputRef}
+                        style={{ display: 'none' }}
+                        accept='.pdf'
+                    />
+                </Col>
+            </Row>
+        </Container>
+    );
+}
